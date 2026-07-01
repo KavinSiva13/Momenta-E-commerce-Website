f38172211813346e7e6fd3281d5a376d4feb5aef
KavinSiva13 <kavinsivaks2003@gmail.com>
2026-07-01 19:24:05 +0530

    first commit

---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,50 @@
+// import { useEffect, useState } from 'react';
+// import {Fragment} from 'react'
+// import ProductCard from '../components/ProductCard';
+
+// export default function Home(){
+//     const [products, setProducts] = useState([]);
+
+//     useEffect(() => {
+//         fetch(process.env.REACT_APP_API_URL+'/products')
+//         .then(res => res.json())
+//         .then( res => setProducts(res.products))
+//     },[])
+
+//     return <Fragment>
+
+//     <h1 id="products_heading">Latest Products</h1>
+
+//     <section id="products" className="container mt-5">
+//       <div className="row">
+//         {products.map(product =><ProductCard product={product}/>)}
+//       </div>
+//     </section>
+
+    
+//     </Fragment>;
+// }
+import {Fragment, useEffect, useState} from 'react'
+import ProductCard from '../components/ProductCard'
+import { useSearchParams } from 'react-router-dom';
+
+export default function Home() {
+    const [products, setProducts] = useState([]);
+    const [searchParams, setSearchParams] =  useSearchParams()
+
+    useEffect(() => {
+        fetch(process.env.REACT_APP_API_URL+'/products?'+searchParams)
+        .then(res => res.json())
+        .then( res => setProducts(res.products))
+    },[searchParams])
+
+    return <Fragment>
+        <h1 id="products_heading">Latest Products</h1>
+
+        <section id="products" className="container mt-5">
+        <div className="row">
+            {products.map(product =><ProductCard product={product} />)} 
+        </div>
+        </section>
+    </Fragment>
+}
