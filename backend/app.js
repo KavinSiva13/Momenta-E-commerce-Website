29c28b4bd4aab9f73c13012330dda7e5d1557955
Kavin Siva <145742969+KavinSiva13@users.noreply.github.com>
2026-07-16 20:30:33 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -1,28 +1,36 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const dotenv = require('dotenv');
-const path = require('path');
-const cors = require('cors');
-const connectDatabase = require('./config/connectDatabase');
-dotenv.config({path: path.join(__dirname, 'config', 'config.env')})
+const dotenv = require("dotenv");
+const path = require("path");
+const cors = require("cors");
 
-const products = require('./routes/products');
-const orders = require('./routes/order');
+dotenv.config(); // Replit Secrets
+
+const connectDatabase = require("./config/connectDatabase");
+
+const products = require("./routes/products");
+const orders = require("./routes/order");
 
 connectDatabase();
 
 app.use(express.json());
 app.use(cors());
-app.use('/api/v1/',products);
-app.use('/api/v1/',orders);
 
-if (process.env.NODE_ENV == 'production') {
-    app.use(express.static(path.join(__dirname, '..', 'frontend',  'build')));
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, '..', 'frontend', 'build', 'index.html'))
-    });
+app.use("/api/v1/", products);
+app.use("/api/v1/", orders);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "..", "frontend", "build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(
+      path.resolve(__dirname, "..", "frontend", "build", "index.html")
+    );
+  });
 }
 
-app.listen(process.env.PORT, () => {
-    console.log(`Server listening to Port ${process.env.PORT} in ${process.env.NODE_ENV}`)
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT} in ${process.env.NODE_ENV}`);
 });
