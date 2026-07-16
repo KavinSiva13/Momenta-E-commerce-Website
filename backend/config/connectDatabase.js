fc84971a363d2073b30944c935bf99b23c85b568
Kavin Siva <145742969+KavinSiva13@users.noreply.github.com>
2026-07-16 20:37:59 +0530

    Change DB_URL to DB_URI and update log messages
    Updated database connection URI variable and modified log messages for success and failure.

--- backend/config/connectDatabase.js
+++ backend/config/connectDatabase.js
@@ -1,12 +1,30 @@
+// const mongoose = require("mongoose");
+
+// const connectDatabase = async () => {
+//   try {
+//     const con = await mongoose.connect(process.env.DB_URL);
+
+//     console.log(`MongoDB Connected: ${con.connection.host}`);
+//   } catch (error) {
+//     console.error("MongoDB Connection Failed!");
+//     console.error(error.message);
+//     process.exit(1);
+//   }
+// };
+
+// module.exports = connectDatabase;
+
+
+
 const mongoose = require("mongoose");
 
 const connectDatabase = async () => {
   try {
-    const con = await mongoose.connect(process.env.DB_URL);
+    const con = await mongoose.connect(process.env.DB_URI);
 
-    console.log(`MongoDB Connected: ${con.connection.host}`);
+    console.log(`✅ MongoDB Connected: ${con.connection.host}`);
   } catch (error) {
-    console.error("MongoDB Connection Failed!");
+    console.error("❌ MongoDB Connection Failed!");
     console.error(error.message);
     process.exit(1);
   }
